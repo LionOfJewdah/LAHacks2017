34e5ad3d43723d8464d6a54ec683c415c03d6680
David Paul Silverstone <dsilvers@usc.edu>
2017-04-01 12:32:24 -0700

    arrays

--- Cube.hpp
+++ Cube.hpp
@@ -24,6 +24,9 @@ private:
         OG = 9,  GR = 10, RB = 11, BO = 12,
         GY = 14, RY = 16, BY = 18, OY = 20
     };
+    corner mCorners[8];
+    edge_piece mEdges[12];
+
 public:
     enum { SOLVED_CUBE = false, RANDOM_CUBE = true };
     Cube();
