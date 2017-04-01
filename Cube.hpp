89063a6291f8c783a3e66a4343da6e00e029bde0
David Paul Silverstone <dsilvers@usc.edu>
2017-04-01 12:07:17 -0700

    Corner enum

--- Cube.hpp
+++ Cube.hpp
@@ -15,8 +15,9 @@
 class Cube {
 private:
     enum corner {
-
-    }
+        WOG = 1,  WGR = 3,  WRB = 5,  WBO = 7,
+        YOG = 13, YGR = 15, YRB = 17, YBO = 19
+    };
 public:
     Cube();
     ~Cube();
