d7c5a64c9bf70fb84830ef8cb38a7891a124a12c
David Paul Silverstone <dsilvers@usc.edu>
2017-04-01 12:25:35 -0700

    Enums are in

--- Cube.hpp
+++ Cube.hpp
@@ -18,7 +18,14 @@ private:
         WOG = 1,  WGR = 3,  WRB = 5,  WBO = 7,
         YOG = 13, YGR = 15, YRB = 17, YBO = 19
     };
+
+    enum edge_piece {
+        WG = 2,  WR = 4,  WB = 6,  WO = 8,
+        OG = 9,  GR = 10, RB = 11, BO = 12,
+        GY = 14, RY = 16, BY = 18, OY = 20
+    };
 public:
+    enum { SOLVED_CUBE = false, RANDOM_CUBE = true };
     Cube();
     ~Cube();
 };
