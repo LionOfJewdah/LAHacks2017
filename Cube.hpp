399c3b86973ce2935627c1f5d6bcd1a094839038
David Paul Silverstone <dsilvers@usc.edu>
2017-04-01 13:52:19 -0700

    add stuff

--- Cube.hpp
+++ Cube.hpp
@@ -42,6 +42,25 @@ public:
 
     bool is_solved() const;
     bool operator==(const Cube& rhs) const;
+
+    Cube& turnUp();
+    Cube& turnUp2();
+    Cube& turnUpI();
+    Cube& turnDown();
+    Cube& turnDown2();
+    Cube& turnDownI();
+    Cube& turnFront();
+    Cube& turnFront2();
+    Cube& turnFrontI();
+    Cube& turnBack();
+    Cube& turnBack2();
+    Cube& turnBackI();
+    Cube& turnLeft();
+    Cube& turnLeft2();
+    Cube& turnLeftI();
+    Cube& turnRight();
+    Cube& turnRight2();
+    Cube& turnRightI();
 };
 
 
