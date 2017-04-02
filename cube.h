5ef2782dabc75cf2bbe101e7eb396b235973c7d5
Jake Leventhal <jglevent@usc.edu>
2017-04-02 06:09:32 -0700

    completed (kind of) cube, cubeCoordinates. TODO: finish cubiecube, main, util, search, tools

--- cube.h
+++ cube.h
@@ -1,7 +1,3 @@
-//  LAHacks2017
-//
-//  Created by Jake Leventhal on Sat, Apr 1st, 2017.
-
 #ifndef CUBE_H
 #define CUBE_H
 
@@ -32,23 +28,23 @@ void halfTurn(T& a, T& b, T& c, T& d)
 // A class representing the states of a rubix cube
 class Cube {
 public:
-	enum : bool { SOLVED_CUBE = false, RANDOM_CUBE = true };
+    enum : bool { SOLVED_CUBE = false, RANDOM_CUBE = true };
 
-	enum corner : int {
-		WOG = 1,  WGR = 3,  WRB = 5,  WBO = 7,
-		YOG = 13, YGR = 15, YRB = 17, YBO = 19,
+    enum corner : int {
+        WOG = 1,  WGR = 3,  WRB = 5,  WBO = 7,
+        YOG = 13, YGR = 15, YRB = 17, YBO = 19,
         ULF = 1,  UFR = 3,  URB = 5,  UBL = 7,
         DFL = 13, DRF = 15, DBR = 17, DLB = 19
-	};
+    };
 
-	enum edge_piece : int {
-		WG = 2,  WR = 4,  WB = 6,  WO = 8,
+    enum edge_piece : int {
+        WG = 2,  WR = 4,  WB = 6,  WO = 8,
         OG = 9,  GR = 10, RB = 11, BO = 12,
-		GY = 14, RY = 16, BY = 18, OY = 20,
+        GY = 14, RY = 16, BY = 18, OY = 20,
         UF = 2,  UR = 4,  UB = 6,  UL = 8,
         FL = 9,  FR = 10, BR = 11, BL = 12,
-		DF = 14, DR = 16, DB = 18, DL = 20
-	};
+        DF = 14, DR = 16, DB = 18, DL = 20
+    };
 private:
     struct corner_o {
         corner co;
@@ -66,17 +62,17 @@ private:
     edge_o mEdges[12];
     bool _isSolved;
     void checkSolved();
-	using Rot = Cube& (Cube::*)(void);
-	void randomPermute();
-    Cube* 
+    using Rot = Cube& (Cube::*)(void);
+    void randomPermute();
+    
 public:
     Cube();
-	Cube(bool);
+    Cube(bool);
     Cube(const int (&_corners)[8], const int (&_edges)[12]);
-	Cube(const Cube&) = default;
-	Cube(Cube&&) = default;
+    Cube(const Cube&) = default;
+    Cube(Cube&&) = default;
     Cube& operator=(Cube&&) = default;
-	Cube& operator=(const Cube&) = default;
+    Cube& operator=(const Cube&) = default;
     ~Cube() = default;
     const corner  (& get_corners() const)[8] ;
     const edge_piece (& get_edges() const)[12];
---
+++ search.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+class Search {
+public:
+    
+private:
+    
+public:
+    Search();
+    ~Search();
+};
+
+#endif
