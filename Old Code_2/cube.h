5c8c6da8d7454da63e89f278d86bd9c3b775a61e
David Paul Silverstone <dsilvers@usc.edu>
2017-04-02 11:09:44 -0700

    m

--- Old Code_2/cube.h
+++ Old Code_2/cube.h
@@ -1,3 +1,7 @@
+//  LAHacks2017
+//
+//  Created by Jake Leventhal on Sat, Apr 1st, 2017.
+
 #ifndef CUBE_H
 #define CUBE_H
 
@@ -28,24 +32,20 @@ void halfTurn(T& a, T& b, T& c, T& d)
 // A class representing the states of a rubix cube
 class Cube {
 public:
-    enum : bool { SOLVED_CUBE = false, RANDOM_CUBE = true };
+	enum : bool { SOLVED_CUBE = false, RANDOM_CUBE = true };
 
-    enum corner : int {
-        WOG = 1,  WGR = 3,  WRB = 5,  WBO = 7,
-        YOG = 13, YGR = 15, YRB = 17, YBO = 19,
-        ULF = 1,  UFR = 3,  URB = 5,  UBL = 7,
-        DFL = 13, DRF = 15, DBR = 17, DLB = 19
-    };
+	enum corner : int{
+		WOG = 1,  WGR = 3,  WRB = 5,  WBO = 7,
+		YOG = 13, YGR = 15, YRB = 17, YBO = 19
+	};
 
-    enum edge_piece : int {
-        WG = 2,  WR = 4,  WB = 6,  WO = 8,
-        OG = 9,  GR = 10, RB = 11, BO = 12,
-        GY = 14, RY = 16, BY = 18, OY = 20,
-        UF = 2,  UR = 4,  UB = 6,  UL = 8,
-        FL = 9,  FR = 10, BR = 11, BL = 12,
-        DF = 14, DR = 16, DB = 18, DL = 20
-    };
+	enum edge_piece : int {
+		WG = 2,  WR = 4,  WB = 6,  WO = 8,
+		OG = 9,  GR = 10, RB = 11, BO = 12,
+		GY = 14, RY = 16, BY = 18, OY = 20
+	};
 private:
+<<<<<<< HEAD
     struct corner_o {
         corner co;
         char o;
@@ -85,14 +85,22 @@ private:
     using Rot = Cube& (Cube::*)(void);
     void randomPermute();
 
+=======
+    corner mCorners[8];
+    edge_piece mEdges[12];
+    bool _isSolved;
+    void checkSolved();
+	using Rot = Cube& (Cube::*)(void);
+	void randomPermute();
+>>>>>>> 0f95f2cd30f3eab5d586316fa6ec684c20f04379
 public:
     Cube();
-    Cube(bool);
+	Cube(bool);
     Cube(const int (&_corners)[8], const int (&_edges)[12]);
-    Cube(const Cube&) = default;
-    Cube(Cube&&) = default;
+	Cube(const Cube&) = default;
+	Cube(Cube&&) = default;
     Cube& operator=(Cube&&) = default;
-    Cube& operator=(const Cube&) = default;
+	Cube& operator=(const Cube&) = default;
     ~Cube() = default;
     const corner_o  (& get_corners() const)[8] ;
     const edge_o    (& get_edges() const)[12];
