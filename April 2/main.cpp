26386dc1ce6d1bb01cca6323196d76ab3e3657fd
Jake Leventhal <jglevent@usc.edu>
2017-04-02 09:41:47 -0700

    modified main

--- April 2/main.cpp
+++ April 2/main.cpp
@@ -17,58 +17,61 @@ int main(int argc, char* argv[])
 
 	string r = Tools.randomCube();
 	
-	private void solveCube(ActionEvent evt) {
-		string cubeState;
+	string cubeState;
 
-		for (int i = 0; i < 54; i++) {
-			s += 'B'; // default initialization
-		}
+	for (int i = 0; i < 54; i++) {
+		s += 'B'; // default initialization
+	}
 
-		int mask = 0;
-		mask |= useSeparator ? Search.USE_SEPARATOR : 0;
-		mask |= inverse ? Search.INVERSE_SOLUTION : 0;
-		mask |= showLength ? Search.APPEND_LENGTH : 0;
-		long t = System.nanoTime();
-		String result = search.solution(cubeString, maxDepth, 100, 0, mask);;
-		long n_probe = search.numberOfProbes();
-		// ++++++++++++++++++++++++ Call Search.solution method from package org.kociemba.twophase ++++++++++++++++++++++++
-		while (result.startsWith("Error 8") && ((System.nanoTime() - t) < maxTime * 1.0e9)) {
-			result = search.next(100, 0, mask);
-			n_probe += search.numberOfProbes();
-		}
-		t = System.nanoTime() - t;
+	int mask = 0;
+	mask |= useSeparator ? Search.USE_SEPARATOR : 0;
+	mask |= inverse ? Search.INVERSE_SOLUTION : 0;
+	mask |= showLength ? Search.APPEND_LENGTH : 0;
+	long t = time(0);
+	//string result = search.solution(cubeString, maxDepth, 100, 0, mask);
+	string result = search.solution(cubeString, maxDepth, 100, 0, 1);
+	long n_probe = search.numberOfProbes();
+	// ++++++++++++++++++++++++ Call Search.solution method from package org.kociemba.twophase ++++++++++++++++++++++++
+	while (result.startsWith("Error 8") && ((time(0) - t) < maxTime * 1.0e9)) {
+		result = search.next(100, 0, mask);
+		n_probe += search.numberOfProbes();
+	}
+	t = time(0) - t;
 
-		// +++++++++++++++++++ Replace the error messages with more meaningful ones in your language ++++++++++++++++++++++
-		if (result.contains("Error")) {
-			switch (result.charAt(result.length() - 1)) {
-			case '1':
-				result = "There are not exactly nine facelets of each color!";
-				break;
-			case '2':
-				result = "Not all 12 edges exist exactly once!";
-				break;
-			case '3':
-				result = "Flip error: One edge has to be flipped!";
-				break;
-			case '4':
-				result = "Not all 8 corners exist exactly once!";
-				break;
-			case '5':
-				result = "Twist error: One corner has to be twisted!";
-				break;
-			case '6':
-				result = "Parity error: Two corners or two edges have to be exchanged!";
-				break;
-			case '7':
-				result = "No solution exists for the given maximum move number!";
-				break;
-			case '8':
-				result = "Timeout, no solution found within given maximum time!";
-				break;
-			}
-			JOptionPane.showMessageDialog(null, result, Double.toString((t / 1000) / 1000.0) + " ms | " + n_probe + " probes", JOptionPane.INFORMATION_MESSAGE);
-		} else {
-			int solLen = (result.length() - (useSeparator ? 3 : 0) - (showLength ? 4 : 0)) / 3;
+	// +++++++++++++++++++ Replace the error messages with more meaningful ones in your language ++++++++++++++++++++++
+	if (result.contains("Error")) {
+		switch (result.charAt(result.length() - 1)) {
+		case '1':
+			result = "There are not exactly nine facelets of each color!";
+			break;
+		case '2':
+			result = "Not all 12 edges exist exactly once!";
+			break;
+		case '3':
+			result = "Flip error: One edge has to be flipped!";
+			break;
+		case '4':
+			result = "Not all 8 corners exist exactly once!";
+			break;
+		case '5':
+			result = "Twist error: One corner has to be twisted!";
+			break;
+		case '6':
+			result = "Parity error: Two corners or two edges have to be exchanged!";
+			break;
+		case '7':
+			result = "No solution exists for the given maximum move number!";
+			break;
+		case '8':
+			result = "Timeout, no solution found within given maximum time!";
+			break;
 		}
+		JOptionPane.showMessageDialog(null, result, Double.toString((t / 1000) / 1000.0) + " ms | " + n_probe + " probes", JOptionPane.INFORMATION_MESSAGE);
+	} else {
+		int solLen = (result.length() - (useSeparator ? 3 : 0) - (showLength ? 4 : 0)) / 3;
+		spinnerMaxMoves.setValue(solLen - 1);
+		jTextPane1.setText(String.format("%s\n" /*, %s ms, %d probes\n*/, result/*, Double.toString((t / 1000) / 1000.0), n_probe*/) + jTextPane1.getText());
+		jTextPane1.requestFocusInWindow();
+		jTextPane1.select(0, result.length());
 	}
 }
