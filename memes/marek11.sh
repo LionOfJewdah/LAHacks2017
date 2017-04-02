70fe5cdb682a06c4e06b921835c53355b7783447
David Paul Silverstone <dsilvers@usc.edu>
2017-04-02 02:22:58 -0700

    dank memes

--- memes/marek11.sh
+++
@@ -1,2 +0,0 @@
-CUBE=`echo $1 | sed "s/O/P/g"`
-./rubcli localhost 5026 $CUBE | sed "s/ //g" 
--- memes/master.sh
+++
@@ -1,9 +0,0 @@
-PROGRAM="$1"
-INPUT="$2"
-OUTPUT="$3"
-while read CUBE; do
-  /usr/bin/time -p $PROGRAM $CUBE > out.$$ 2> time.$$
-  echo $CUBE $(cat out.$$) $(grep real time.$$ | tr -dC '0-9.' ) >> $OUTPUT
-  rm out.$$ time.$$
-done < $INPUT
-
